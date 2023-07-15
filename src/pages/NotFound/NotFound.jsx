1c0c1748c7d8dade025a08bcfcf51dae17688dbd
ClementAver <clement.aver@yahoo.fr>
2023-07-15 23:36:49 +0200

    Adds custom 404.

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <h1>not found</h1>;
+  return (
+    <main className="not-found">
+      <Link
+        className="main-nav-logo"
+        to="/"
+      >
+        <img
+          className="main-nav-logo-image"
+          src="./assets/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </Link>
+      <h1>Oups, cette page n'existe pas.</h1>
+      <Link to="/">Revenir à l'accueil</Link>
+    </main>
+  );
 }
